dd378ec604fc7d657f2ddbcc535e6d034c600693
PHILIPP111007 <r.phil@yandex.ru>
2023-12-03 13:25:55 +0300

    updated Modals rendering + closing by ESC btn

--- frontend/src/pages/components/Modal.jsx
+++ frontend/src/pages/components/Modal.jsx
@@ -1,18 +1,35 @@
+import { useMemo } from "react"
 import cl from "./styles/Modal.module.css"
 
 export default function Modal(props) {
 
-    const rootClass = [cl.Modal]
-
-    if (props.modal) {
-        rootClass.push(cl.active)
+    function closeModalByESC(e) {
+        if (e.key === "Escape" && props.modal) { // escape key maps to keycode `27`
+            props.setModal(false)
+            document.removeEventListener("keyup", closeModalByESC)
+        }
     }
 
-    return (
-        <div className={rootClass.join(" ")} onClick={() => props.setModal(false) } >
-            <div className={cl.ModalContent} onClick={e => e.stopPropagation()} >
-                {props.children}
-            </div>
-        </div>
-    )
+    const showModalChildren = useMemo(() => {
+        document.addEventListener("keyup", closeModalByESC)
+
+        if (props.modal === true) {
+            const rootClass = [cl.Modal]
+            rootClass.push(cl.active)
+
+            return (
+                <div className={rootClass.join(" ")} onClick={() => {
+                    props.setModal(false)
+                    document.removeEventListener("keyup", closeModalByESC)
+                }} >
+                    <div className={cl.ModalContent} onClick={e => e.stopPropagation()} >
+                        {props.children}
+                    </div>
+                </div>
+            )
+        }
+
+    }, [props.modal])
+
+    return showModalChildren
 }
