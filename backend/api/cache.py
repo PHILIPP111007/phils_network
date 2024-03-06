85304644042cfdb6b9bbeebdb07b0cd5c8e5f613
Philipp Roshchin <r.phil@yandex.ru>
2024-03-06 20:23:30 +0300

    fix: added USER_CACHE_TIME

--- backend/api/cache.py
+++ backend/api/cache.py
@@ -1,9 +1,12 @@
 from django.core.cache import cache
 
 
+USER_CACHE_TIME = 60 * 60 * 24 * 7
+
+
 def get_user_cache(username):
 	user_cache: dict = cache.get(username)
 	if not user_cache:
-		cache.set(username, {}, 60 * 60 * 24 * 7)
+		cache.set(username, {}, USER_CACHE_TIME)
 		return {}
 	return user_cache
