4f6daeb1ebe268e1e560eec7da36bdc1b31fc21a
Philipp Roshchin <r.phil@yandex.ru>
2024-03-15 01:12:45 +0300

    updated enums

--- backend/api/enums.py
+++ backend/api/enums.py
@@ -1,21 +1,19 @@
-import enum
+from enum import IntEnum
 
 
-class SubscriberStatus(enum.Enum):
-	"""Implements subscriber status."""
-
+class SubscriberStatus(IntEnum):
 	IS_FRIEND = 1
 	ME_SUBSCRIBER = 2
 	HE_SUBSCRIBER = 3
 	NO_DATA = 4
 
 
-class DeleteOption(enum.Enum):
+class DeleteOption(IntEnum):
 	DELETE_FRIEND = 1
 	DELETE_SUBSCRIBER = 2
 
 
-class FilterOption(enum.Enum):
+class FilterOption(IntEnum):
 	FRIENDS = 1
 	SUBSCRIPTIONS = 2
 	SUBSCRIBERS = 3
