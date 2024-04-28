905e491c54d97e025634888cba5cd5ed3e58e0c5
Philipp Roshchin <r.phil@yandex.ru>
2024-04-28 19:43:46 +0300

    optimization

--- Services/django/settings/server_conf.py
+++ Services/django/settings/server_conf.py
@@ -7,12 +7,12 @@ import tomllib
 ENV_FILE_PATH: str = "pyproject.toml"
 
 
-def _set_args(env_file: str, app_part: dict, app_part_name: str = "") -> None:
+def _set_args(app_part: dict, app_part_name: str = "") -> None:
 	if app_part:
 		for key, val in app_part.items():
 			os.environ.setdefault(key, val)
 	else:
-		print(f'"{env_file}" file does not contain {app_part_name} variables.')
+		print(f'"{ENV_FILE_PATH}" file does not contain {app_part_name} variables.')
 
 
 def read_and_set_env() -> None:
@@ -24,10 +24,8 @@ def read_and_set_env() -> None:
 			app: dict = toml_content.get("app", "")
 			app_settings: dict = toml_content.get("app-settings", "")
 
-		_set_args(env_file=ENV_FILE_PATH, app_part=app, app_part_name="app")
-		_set_args(
-			env_file=ENV_FILE_PATH, app_part=app_settings, app_part_name="app_settings"
-		)
+		_set_args(app_part=app, app_part_name="app")
+		_set_args(app_part=app_settings, app_part_name="app_settings")
 
 	else:
 		print(f'You do not have "{ENV_FILE_PATH}" file.')
