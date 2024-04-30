00c6b5a333077bb21f784dd603fa0c4910b3f4ae
Philipp Roshchin <r.phil@yandex.ru>
2024-04-30 16:35:02 +0300

    added env vars

--- Front/react/src/data/constants.js
+++ Front/react/src/data/constants.js
@@ -1,5 +1,5 @@
-var SERVER_HOST = "0.0.0.0"
-var SERVER_PORT = "8000"
+var SERVER_HOST = process.env.REACT_APP_SERVER_HOST || "0.0.0.0"
+var SERVER_PORT = process.env.REACT_APP_SERVER_PORT || "8000"
 
 export var FETCH_URL = `http://${SERVER_HOST}:${SERVER_PORT}/`
 export var WEBSOCKET_URL = `ws://${SERVER_HOST}:${SERVER_PORT}/ws/`
