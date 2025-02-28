6e29f168f74dddb5593a8daf3973e46c59be0331
Philipp Roshchin <r.phil@yandex.ru>
2025-02-28 22:46:12 +0300

    renaming

--- Front/react/src/data/constants.js
+++ Front/react/src/data/constants.js
@@ -1,7 +1,7 @@
-var SERVER_HostIp = "0.0.0.0"
-var SERVER_HostPort = "80"
+var SERVER_HOST = "0.0.0.0"
+var SERVER_PORT = "80"
 
-export var FETCH_URL = `http://${SERVER_HostIp}:${SERVER_HostPort}/api/v1/`
-export var WEBSOCKET_URL = `ws://${SERVER_HostIp}:${SERVER_HostPort}/ws/`
+export var FETCH_URL = `http://${SERVER_HOST}:${SERVER_PORT}/api/v1/`
+export var WEBSOCKET_URL = `ws://${SERVER_HOST}:${SERVER_PORT}/ws/`
 
 export var ROOT_ELEMENT_THEME = document.getElementsByTagName("html")[0]
