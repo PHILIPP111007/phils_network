da16f2e0aeb4c393f1ee65abc9d22380a935c23d
Philipp Roshchin <r.phil@yandex.ru>
2025-03-03 17:29:25 +0300

    added relations

--- Services/fastapi/app/models/subscriber.py
+++ Services/fastapi/app/models/subscriber.py
@@ -1,12 +1,27 @@
 __all__ = ["Blog"]
 
 
-from sqlmodel import Field, SQLModel
+from sqlalchemy.orm import RelationshipProperty
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class Subscriber(SQLModel, table=True):
     __tablename__ = "app_subscriber"
 
     id: int = Field(primary_key=True)
-    subscribe_id: int = Field(foreign_key="auth_user.id")
     user_id: int = Field(foreign_key="auth_user.id")
+    subscribe_id: int = Field(foreign_key="auth_user.id")
+    user: "User" = Relationship(
+        sa_relationship=RelationshipProperty(
+            "User",
+            back_populates="subscriber_user",
+            foreign_keys="[Subscriber.user_id]",
+        ),
+    )
+    subscriber: "User" = Relationship(
+        sa_relationship=RelationshipProperty(
+            "User",
+            back_populates="subscriber_subscriber",
+            foreign_keys="[Subscriber.subscribe_id]",
+        ),
+    )
--- Services/fastapi/app/models/user.py
+++ Services/fastapi/app/models/user.py
@@ -17,3 +17,17 @@ class User(SQLModel, table=True):
     tokens: list["Token"] = Relationship(back_populates="user")
     online_statuses: list["OnlineStatus"] = Relationship(back_populates="user")
     blogs: list["Blog"] = Relationship(back_populates="user")
+    subscriber_user: list["Subscriber"] = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={
+            "primaryjoin": "Subscriber.user_id==User.id",
+            "lazy": "joined",
+        },
+    )
+    subscriber_subscriber: list["Subscriber"] = Relationship(
+        back_populates="subscriber",
+        sa_relationship_kwargs={
+            "primaryjoin": "Subscriber.subscribe_id==User.id",
+            "lazy": "joined",
+        },
+    )
