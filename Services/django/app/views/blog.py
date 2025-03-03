149ac36397f96bd573f866189e264cc5c29a71ee
Philipp Roshchin <r.phil@yandex.ru>
2025-03-03 23:08:25 +0300

    removed put function

--- Services/django/app/views/blog.py
+++ Services/django/app/views/blog.py
@@ -35,39 +35,6 @@ class BlogAPIView(APIView):
 			{"ok": True, "post": serializer.data}, status=status.HTTP_200_OK
 		)
 
-	def put(self, request: Request, **kwargs) -> Response:
-		"""Update post text."""
-
-		self.check_permissions(request=request)
-		pk = kwargs.get("pk", None)
-		if pk is None:
-			return Response(
-				{"ok": False, "error": "You didn't submit a post id."},
-				status=status.HTTP_404_NOT_FOUND,
-			)
-
-		post = self.service_class.filter(pk=pk).first()
-		if post is None:
-			return Response(
-				{"ok": False, "error": "Not found the post."},
-				status=status.HTTP_404_NOT_FOUND,
-			)
-
-		serializer = self.serializer_class(data=request.data, instance=post)
-		serializer.is_valid(raise_exception=True)
-		user = serializer.validated_data.get("user")
-		self.check_object_permissions(request=request, obj=user)
-		serializer.save()
-
-		user_cache: dict = get_user_cache(username=request.user)
-		if user_cache:
-			user_cache["blog"] = []
-			cache.set(request.user, user_cache)
-
-		return Response(
-			{"ok": True, "post": serializer.data}, status=status.HTTP_200_OK
-		)
-
 	def delete(self, request: Request, **kwargs) -> Response:
 		self.check_permissions(request=request)
 		pk = kwargs.get("pk", None)
