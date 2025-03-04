a47dd50475396b75668c1a3ebaffd3a13262298a
Philipp Roshchin <r.phil@yandex.ru>
2025-03-04 15:10:00 +0300

    added post_find_user

--- Services/fastapi/app/main.py
+++ Services/fastapi/app/main.py
@@ -396,3 +396,98 @@ async def delete_subscriber(
     session.commit()
 
     return {"ok": True}
+
+
+#######################################
+# FindUser ############################
+#######################################
+
+
+@app.post("/api/v2/find_user/")
+async def post_find_user(session: SessionDep, request: Request):
+    if not request.state.user:
+        return {"ok": False, "error": "Can not authenticate."}
+
+    body = await request.body()
+    body: dict = json.loads(body)
+    username = body["username"]
+
+    find_users = None
+    if username:
+        find_users = (
+            session.exec(
+                select(User).where(
+                    User.username.contains(username), User.id != request.state.user.id
+                )
+            )
+            .unique()
+            .all()
+        )
+    else:
+        first_name: str | None = body["first_name"]
+        last_name: str | None = body["last_name"]
+
+        if not (first_name or last_name):
+            return {"ok": False}
+
+        if first_name:
+            query_1 = (
+                session.exec(
+                    select(User).where(
+                        User.first_name.contains(first_name),
+                        User.id != request.state.user.id,
+                    )
+                )
+                .unique()
+                .all()
+            )
+
+        if last_name:
+            query_2 = (
+                session.exec(
+                    select(User).where(
+                        User.last_name.contains(last_name),
+                        User.id != request.state.user.id,
+                    )
+                )
+                .unique()
+                .all()
+            )
+
+        if first_name and last_name:
+            find_users = []
+            for user in query_1:
+                find_users.append(user)
+            for user in query_2:
+                find_users.append(user)
+        elif first_name:
+            find_users = query_1
+        elif last_name:
+            find_users = query_2
+
+    if not find_users:
+        return {"ok": False, "error": "Not found users."}
+
+    users = []
+    for find_user in find_users:
+        is_online = (
+            session.exec(
+                select(OnlineStatus).where(OnlineStatus.user_id == find_user.id)
+            )
+            .unique()
+            .all()
+        )
+        if is_online:
+            is_online = is_online[0]
+        else:
+            is_online = False
+
+        user = {
+            "username": find_user.username,
+            "first_name": find_user.first_name,
+            "last_name": find_user.last_name,
+            "is_online": is_online,
+        }
+        users.append(user)
+
+    return {"ok": True, "users": users}
