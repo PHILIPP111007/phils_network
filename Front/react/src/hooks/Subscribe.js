aff01e5622a299595ccc6152976f2d9f4b8de9ed
Philipp Roshchin <r.phil@yandex.ru>
2025-03-04 14:17:21 +0300

    replaced api version

--- Front/react/src/hooks/Subscribe.js
+++ Front/react/src/hooks/Subscribe.js
@@ -5,7 +5,7 @@ export default class Subscribe {
 
     static deleteFriend(props) {
         var body = { option: DeleteOptionEnum.DELETE_FRIEND }
-        Fetch({ action: `api/v1/subscriber/${props.pk}/`, method: HttpMethod.DELETE, body: body })
+        Fetch({ action: `api/v2/subscriber/${props.pk}/`, method: HttpMethod.DELETE, body: body })
             .then(() => {
                 props.setStatus(undefined)
             })
@@ -13,14 +13,14 @@ export default class Subscribe {
 
     static deleteSubscriber(props) {
         var body = { option: DeleteOptionEnum.DELETE_SUBSCRIBER }
-        Fetch({ action: `api/v1/subscriber/${props.pk}/`, method: HttpMethod.DELETE, body: body })
+        Fetch({ action: `api/v2/subscriber/${props.pk}/`, method: HttpMethod.DELETE, body: body })
             .then(() => {
                 props.setStatus(undefined)
             })
     }
 
     static addSubscription(props) {
-        Fetch({ action: `api/v1/subscriber/${props.pk}/`, method: HttpMethod.POST })
+        Fetch({ action: `api/v2/subscriber/${props.pk}/`, method: HttpMethod.POST })
             .then(() => {
                 props.setStatus(undefined)
             })
