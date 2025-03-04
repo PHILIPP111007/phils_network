12be9013c9dc7bdb5725fdd92e2067a5623d7d80
Philipp Roshchin <r.phil@yandex.ru>
2025-03-04 14:19:02 +0300

    rewrited type annotations && added get_subscriber and post_subscriber and delete_subscriber

--- Services/fastapi/app/main.py
+++ Services/fastapi/app/main.py
@@ -6,7 +6,7 @@ from sqlmodel import Session, delete, select
 
 from app.constants import DATETIME_FORMAT, POSTS_TO_LOAD
 from app.database import SessionDep, engine
-from app.enums import SubscriberStatus
+from app.enums import DeleteOption, SubscriberStatus
 from app.models import Blog, OnlineStatus, Subscriber, Token, User
 
 app = FastAPI(
@@ -78,7 +78,7 @@ async def post_online_status(session: SessionDep, request: Request) -> dict[str,
 @app.get("/api/v2/blog/{username}/{loaded_posts}/")
 async def get_blog(
     session: SessionDep, request: Request, username: str, loaded_posts: int
-) -> list[Blog]:
+):
     async def _filter(user_id: int, subscribe_id: int) -> list[Subscriber]:
         return (
             session.exec(
@@ -152,7 +152,7 @@ async def get_blog(
 
 
 @app.put("/api/v2/blog/{id}")
-async def put_blog(session: SessionDep, request: Request, id: int) -> Blog:
+async def put_blog(session: SessionDep, request: Request, id: int):
     if not request.state.user:
         return {"ok": False, "error": "Can not authenticate."}
 
@@ -163,7 +163,8 @@ async def put_blog(session: SessionDep, request: Request, id: int) -> Blog:
         return {"ok": False, "error": "Access denied."}
 
     body = await request.body()
-    post.content = json.loads(body)["content"]
+    body: dict = json.loads(body)
+    post.content = body["content"]
     post.changed = True
 
     session.add(post)
@@ -173,12 +174,12 @@ async def put_blog(session: SessionDep, request: Request, id: int) -> Blog:
 
 
 @app.post("/api/v2/blog/")
-async def post_blog(session: SessionDep, request: Request) -> Blog:
+async def post_blog(session: SessionDep, request: Request):
     if not request.state.user:
         return {"ok": False, "error": "Can not authenticate."}
 
     body = await request.body()
-    body = json.loads(body)
+    body: dict = json.loads(body)
 
     post = Blog(
         user_id=body["user"],
@@ -222,9 +223,7 @@ async def delete_blog(
 
 
 @app.get("/api/v2/news/{loaded_posts}")
-async def get_news(
-    session: SessionDep, request: Request, loaded_posts: int
-) -> list[Blog]:
+async def get_news(session: SessionDep, request: Request, loaded_posts: int):
     async def _get_friends(id):
         set_1 = (
             session.exec(
@@ -292,3 +291,108 @@ async def get_news(
         )
 
     return {"ok": True, "posts": posts}
+
+
+#########################################
+# Subscriber ############################
+#########################################
+
+
+@app.get("/api/v2/subscriber/{id}/")
+async def get_subscriber(
+    session: SessionDep, request: Request, id: int
+) -> dict[str, int]:
+    async def _filter(user_id: int, subscribe_id: int) -> list[Subscriber]:
+        return (
+            session.exec(
+                select(Subscriber).where(
+                    Subscriber.user_id == user_id,
+                    Subscriber.subscribe_id == subscribe_id,
+                )
+            )
+            .unique()
+            .all()
+        )
+
+    if not request.state.user:
+        return {"ok": False, "error": "Can not authenticate."}
+
+    user_1 = await _filter(user_id=request.state.user.id, subscribe_id=id)
+    user_2 = await _filter(user_id=id, subscribe_id=request.state.user.id)
+
+    # If we are friends, I can see his blog.
+    if user_1 and user_2:
+        status = SubscriberStatus.IS_FRIEND.value
+    elif user_1:
+        status = SubscriberStatus.ME_SUBSCRIBER.value
+    elif user_2:
+        status = SubscriberStatus.HE_SUBSCRIBER.value
+    else:
+        status = SubscriberStatus.NO_DATA.value
+
+    return {"ok": True, "status": status}
+
+
+@app.post("/api/v2/subscriber/{id}/")
+async def post_subscriber(
+    session: SessionDep, request: Request, id: int
+) -> dict[str, bool]:
+    if not request.state.user:
+        return {"ok": False, "error": "Can not authenticate."}
+
+    subscribe = Subscriber(user_id=request.state.user.id, subscribe_id=id)
+    session.add(subscribe)
+    session.commit()
+
+    return {"ok": True}
+
+
+@app.delete("/api/v2/subscriber/{id}/")
+async def delete_subscriber(
+    session: SessionDep, request: Request, id: int
+) -> dict[str, bool]:
+    if not request.state.user:
+        return {"ok": False, "error": "Can not authenticate."}
+
+    body = await request.body()
+    body: dict = json.loads(body)
+
+    if not body.get("option"):
+        return {"ok": False, "error": "Not provided an option."}
+
+    option = body["option"]
+
+    subscribe = None
+    if option == DeleteOption.DELETE_FRIEND.value:
+        subscribe = (
+            session.exec(
+                select(Subscriber).where(
+                    Subscriber.user_id == request.state.user.id,
+                    Subscriber.subscribe_id == id,
+                )
+            )
+            .unique()
+            .all()
+        )
+        if not subscribe:
+            return {"ok": False, "error": "Not found subscriber."}
+        subscribe = subscribe[0]
+    elif option == DeleteOption.DELETE_SUBSCRIBER.value:
+        subscribe = (
+            session.exec(
+                select(Subscriber).where(
+                    Subscriber.user_id == id,
+                    Subscriber.subscribe_id == request.state.user.id,
+                )
+            )
+            .unique()
+            .all()
+        )
+        if not subscribe:
+            return {"ok": False, "error": "Not found subscriber."}
+        subscribe = subscribe[0]
+
+    session.exec(delete(Subscriber).where(Subscriber.id == subscribe.id))
+    session.commit()
+
+    return {"ok": True}
