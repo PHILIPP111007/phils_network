18395e8f5b07bb5ad65e10d8ac75f83fc99630d0
Philipp Roshchin <r.phil@yandex.ru>
2025-03-05 22:13:56 +0300

    fix

--- Services/django/settings/asgi.py
+++ Services/django/settings/asgi.py
@@ -11,17 +11,6 @@ environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.settings")
 django_asgi_app = get_asgi_application()
 
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter
 
-from app.routing import websocket_urlpatterns
-
-application = ProtocolTypeRouter(
-	{
-		"http": django_asgi_app,
-		"websocket": AllowedHostsOriginValidator(
-			AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-		),
-	}
-)
+application = ProtocolTypeRouter({"http": django_asgi_app})
