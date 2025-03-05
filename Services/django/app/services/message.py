3630cd811be5409a32d5a014be23335d4553e57c
Philipp Roshchin <r.phil@yandex.ru>
2025-03-05 14:59:32 +0300

    removed filter function

--- Services/django/app/services/message.py
+++ Services/django/app/services/message.py
@@ -1,27 +1,7 @@
 from app.models import Message, Room
-from django.conf import settings
-from django.db.models.query import QuerySet
 
 
 class MessageService:
-	@staticmethod
-	def filter(room_id: int, loaded_messages: int) -> QuerySet[Message]:
-		"""Lazy loading of messages for room."""
-
-		messages = (
-			Message.objects.filter(room_id=room_id)
-			.select_related("sender")
-			.only(
-				"text",
-				"timestamp",
-				"sender__username",
-				"sender__first_name",
-				"sender__last_name",
-			)[loaded_messages : loaded_messages + settings.MESSAGES_TO_LOAD]
-		)
-
-		return messages
-
 	@staticmethod
 	def create(room_id: int, sender_id: int, text: str) -> Message:
 		return Message.objects.create(room_id=room_id, sender_id=sender_id, text=text)
