3e0436e3ff561dc13d01fcedf913b84961d56db5
Philipp Roshchin <r.phil@yandex.ru>
2025-03-06 23:10:19 +0300

    updated django admin page

--- Services/django/app/admin.py
+++ Services/django/app/admin.py
@@ -12,9 +12,11 @@ from django.contrib import admin
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-	exclude = ["content"]
+	exclude = ("content",)
 	list_display = ("user", "timestamp")
 	list_filter = ("user",)
+	ordering = ("-timestamp",)
+	search_fields = ("user__username",)
 
 
 @admin.register(Subscriber)
@@ -28,9 +30,19 @@ class SubscriberAdmin(admin.ModelAdmin):
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-	filter_horizontal = ["subscribers"]
-	list_display = ("name", "timestamp")
+	filter_horizontal = ("subscribers",)
+	list_display = ("name", "timestamp", "room_messages", "room_subscribers")
 	list_filter = ("name",)
+	ordering = ("-timestamp",)
+	search_fields = ("name",)
+
+	def room_messages(self, room):
+		messages_count = Message.objects.filter(room=room.id).count()
+		return f"{messages_count}"
+
+	def room_subscribers(self, room):
+		subscribers_count = Room.objects.filter(pk=room.id).first().subscribers.count()
+		return f"{subscribers_count}"
 
 
 @admin.register(RoomCreator)
@@ -43,13 +55,16 @@ class RoomCreatorAdmin(admin.ModelAdmin):
 class RoomInvitationAdmin(admin.ModelAdmin):
 	list_display = ("creator", "to_user", "room", "timestamp")
 	list_filter = ("creator",)
+	ordering = ("-timestamp",)
 
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-	exclude = ["text"]
+	exclude = ("text",)
 	list_display = ("sender", "room", "timestamp")
 	list_filter = ("sender", "room")
+	ordering = ("-timestamp",)
+	search_fields = ("sender__username",)
 
 
 @admin.register(OnlineStatus)
