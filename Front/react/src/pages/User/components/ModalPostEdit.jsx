8d5805c9b06074872c5c534d3f202408f42217f2
Philipp Roshchin <r.phil@yandex.ru>
2025-03-09 17:16:44 +0300

    added placeholder

--- Front/react/src/pages/User/components/ModalPostEdit.jsx
+++ Front/react/src/pages/User/components/ModalPostEdit.jsx
@@ -7,6 +7,7 @@ export default function ModalPostEdit(props) {
             <textarea
                 value={props.mainSets.post.content}
                 maxLength="5000"
+                placeholder="Use markdown to format your text..."
                 onChange={(e) => props.setMainSets({
                     ...props.mainSets,
                     post: { ...props.mainSets.post, content: e.target.value }
