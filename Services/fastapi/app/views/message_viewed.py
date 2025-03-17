b078c27521011ec6345011ddc3832eea61faeada
Philipp Roshchin <r.phil@yandex.ru>
2025-03-17 20:14:49 +0300

    fix

--- Services/fastapi/app/views/message_viewed.py
+++ Services/fastapi/app/views/message_viewed.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request
+from sqlmodel import select
 
 from app.database import SessionDep
 from app.models import MessageViewed
@@ -11,7 +12,17 @@ async def post_message_viewed(session: SessionDep, request: Request, message_id:
 	if not request.state.user:
 		return {"ok": False, "error": "Can not authenticate."}
 
-	message_viewed = MessageViewed(message_id=message_id, user_id=request.state.user.id)
-	session.add(message_viewed)
-	session.commit()
+	message_viewed = session.exec(
+		select(MessageViewed).where(
+			MessageViewed.message_id == message_id,
+			MessageViewed.user_id == request.state.user.id,
+		)
+	).first()
+
+	if not message_viewed:
+		message_viewed = MessageViewed(
+			message_id=message_id, user_id=request.state.user.id
+		)
+		session.add(message_viewed)
+		session.commit()
 	return {"ok": True}
