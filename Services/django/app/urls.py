fd97f41aa87e5f8eb2743ac18a03444625e91804
Philipp Roshchin <r.phil@yandex.ru>
2025-03-19 15:54:16 +0300

    changed comments

--- Services/django/app/urls.py
+++ Services/django/app/urls.py
@@ -1,17 +1,17 @@
 """
 Create user:
-POST http://127.0.0.1:8000/api/auth/users/ {"username": "admin", "password": "123", "email": "email@gmail.com"}
+POST http://127.0.0.1:8000/api/v1/auth/users/ {"username": "admin", "password": "123"}
 
 Get user info:
 headers -> Authorization -> Token d91dacef1757b45259d45372359d4f7c91a856c2
-GET http://127.0.0.1:8000/api/auth/users/me/
+GET http://127.0.0.1:8000/api/v1/auth/users/me/
 
 Get token and authorize (--> {"auth_token":"d91dacef1757b45259d45372359d4f7c91a856c2"}):
-POST http://127.0.0.1:8000/token/token/login/ {"username": "admin", "password": "123"}
+POST http://127.0.0.1:8000/api/v1/token/token/login/ {"username": "admin", "password": "123"}
 
 Logout:
 headers -> Authorization -> Token d91dacef1757b45259d45372359d4f7c91a856c2
-POST http://127.0.0.1:8000/token/token/logout/
+POST http://127.0.0.1:8000/api/v1/token/token/logout/
 """
 
 __all__ = ["urlpatterns"]
