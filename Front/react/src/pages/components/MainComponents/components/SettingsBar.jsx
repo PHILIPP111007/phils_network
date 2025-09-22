c036a1f6d5e54639482ddfed18799ce121b7c21a
Philipp Roshchin <r.phil@yandex.ru>
2025-09-23 01:06:30 +0300

    fixed bug at logout

--- Front/react/src/pages/components/MainComponents/components/SettingsBar.jsx
+++ Front/react/src/pages/components/MainComponents/components/SettingsBar.jsx
@@ -40,8 +40,8 @@ export default function SettingsBar(props) {
     async function logout() {
         await Fetch({ action: "api/v2/online_status/set_false/", method: HttpMethod.POST })
         await Fetch({ action: "api/v1/token/token/logout/", method: HttpMethod.POST })
-        localStorage.clear()
         setIsAuth(false)
+        localStorage.removeItem(CacheKeys.TOKEN)
         setUser({
             id: 0,
             username: "",
