dd7219efefffd03416d075f2e237878458d7fa23
Philipp Roshchin <r.phil@yandex.ru>
2025-09-23 00:48:54 +0300

    updated notifications

--- Front/react/src/pages/components/MainComponents/components/NavBar.jsx
+++ Front/react/src/pages/components/MainComponents/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import "./styles/NavBar.css"
 import { use, useState, useEffect, useRef } from "react"
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
+import toast from "react-hot-toast"
 import { getWebSocketDjango } from "../../../../modules/getWebSocket.js"
 import Fetch from "../../../../API/Fetch.js"
 import { notify } from "../../../../modules/notify.js"
@@ -15,6 +16,7 @@ export default function NavBar() {
     var [unreadMessagesCount, setUnreadMessagesCount] = useState(0)
     var [rooms, setRooms] = useState([])
     var roomSocket = useRef(null)
+    var params = useParams()
 
     async function getUnreadMessagesCount() {
         var data = await Fetch({ action: `api/v2/get_unread_message_count/`, method: HttpMethod.GET })
@@ -39,7 +41,7 @@ export default function NavBar() {
             var socket = getWebSocketDjango({ socket_name: "roomSocketNavBar", path: `chat/${room.id}/` })
             socket.onmessage = (e) => {
                 var data = JSON.parse(e.data)
-                if (user.username !== data.message.sender.username) {
+                if (user.username !== data.message.sender.username && Number(params.room_id) !== room.id) {
                     var msg = ""
                     var username = data.message.sender.username
                     var text = data.message.text
@@ -63,6 +65,7 @@ export default function NavBar() {
                         }
                     }
 
+                    toast.remove()
                     notify(
                         <div className="Notification" >
                             <Link to={`/chats/${user.username}/${room.id}/`}>{msg}</Link>
