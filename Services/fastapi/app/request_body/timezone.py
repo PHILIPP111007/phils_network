1d0b5627cf645cc7561ecebe6d64e8cb11b268cc
Philipp Roshchin <r.phil@yandex.ru>
2025-10-03 15:54:46 +0300

    refactoring

--- Services/fastapi/app/request_body/timezone.py
+++ Services/fastapi/app/request_body/timezone.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
 
+
 class TimezoneData(BaseModel):
-    timezone: str
+	timezone: str
--- Services/fastapi/app/views/timezone.py
+++ Services/fastapi/app/views/timezone.py
@@ -10,17 +10,17 @@ router = APIRouter(tags=["timezone"])
 
 @router.post("/api/v2/timezone/")
 async def post_timezone(session: SessionDep, request: Request, body: TimezoneData):
-    if not request.state.user:
-        return {"ok": False, "error": "Can not authenticate."}
+	if not request.state.user:
+		return {"ok": False, "error": "Can not authenticate."}
 
-    query = await session.exec(select(User).where(User.id == request.state.user.id))
-    user = query.first()
-    if not user:
-        return {"ok": False, "error": "Not found the global user."}
+	query = await session.exec(select(User).where(User.id == request.state.user.id))
+	user = query.first()
+	if not user:
+		return {"ok": False, "error": "Not found the global user."}
 
-    user.user_timezone = body.timezone
+	user.user_timezone = body.timezone
 
-    session.add(user)
-    await session.commit()
+	session.add(user)
+	await session.commit()
 
-    return {"ok": True}
+	return {"ok": True}
