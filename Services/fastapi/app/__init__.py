ccd418c4a7fb3f81285e2ca74ca0a7bcf80186e6
Philipp Roshchin <r.phil@yandex.ru>
2025-10-26 13:39:13 +0300

    fix

--- Services/fastapi/app/__init__.py
+++ Services/fastapi/app/__init__.py
@@ -1,11 +1,12 @@
-from app.constants import BUCKET_NAME
+from app.constants import BUCKET_NAME, TESTING
 from app.s3 import create_bucket, s3
 
-try:
-	for bucket in s3.list_buckets()["Buckets"]:
-		if bucket["Name"] == BUCKET_NAME:
-			break
-	else:
-		create_bucket()
-except Exception:
-	pass
+if TESTING != "1":
+	try:
+		for bucket in s3.list_buckets()["Buckets"]:
+			if bucket["Name"] == BUCKET_NAME:
+				break
+		else:
+			create_bucket()
+	except Exception:
+		pass
