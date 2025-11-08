ee81aa1a21658ff20240e05011780787c777be75
Philipp Roshchin <r.phil@yandex.ru>
2025-11-09 01:21:01 +0300

    removed comment

--- Services/django/granian.sh
+++ Services/django/granian.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # Runs granian
-# Now not supports Django Channels WebSockets
 
 granian settings.asgi:application --interface asginl --ws --host 0.0.0.0 --port 8080 --workers 1 --log --reload
