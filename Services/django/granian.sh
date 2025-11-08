c872e958df7a3f88f74f3c57b8cf2992b2f86468
Philipp Roshchin <r.phil@yandex.ru>
2025-11-08 15:07:23 +0300

    updated granian config

--- Services/django/granian.sh
+++ Services/django/granian.sh
@@ -2,4 +2,4 @@
 
 # Runs granian
 
-granian settings.asgi:application --interface asgi --host 0.0.0.0 --port 8080 --ws --workers 1
+granian settings.asgi:application --interface asginl --ws --host 0.0.0.0 --port 8080 --workers 1 --log --reload
